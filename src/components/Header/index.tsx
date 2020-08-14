3fc4e58eeb20cd6180284f2ca526625aef37cb9d
Clay Clecidor <clecidor@gmail.com>
2020-08-14 12:35:44 -0400

    add className support to Header
    modified:   src/components/Header/index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import classnames from 'classnames';
 import styles from './styles.module.scss';
 
 
 type Props = {
   title?: string;
+  className?: string;
   children: React.ReactNode;
 };
 
-const Header = ({ title, children } : Props) => (
-  <header className={styles.Header}>
+const Header = ({ title, className, children } : Props) => (
+  <header className={classnames(styles.Header, className)}>
     {title && <h1 className={styles.title}>{title}</h1>}
     <p>{children}</p>
   </header>
