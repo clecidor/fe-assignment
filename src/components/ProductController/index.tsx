da2e3e4ae165b15cd0b51dc876706be3882285a2
Clay Clecidor <clecidor@gmail.com>
2020-08-14 13:46:15 -0400

    First pass styling ProductController
    modified:   src/components/ProductController/index.tsx
    modified:   src/components/ProductController/styles.module.scss

--- src/components/ProductController/index.tsx
+++ src/components/ProductController/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from '../Button';
+import Section from '../Section';
 import styles from './styles.module.scss';
 
 
@@ -7,10 +9,26 @@ type Props = {
 };
 
 const ProductController = ({ children } : Props) => (
-  <div className={styles.ProductController}>
-    <strong>ProductController goes here</strong>
+  <Section className={styles.ProductController}>
+    <div className={styles.options}>
+      <label>
+        <strong>Select Appliances:</strong>
+        <select><option>4 Appliances Selected</option></select>
+      </label>
+      <label>
+        <strong>Sort By:</strong>
+        <select><option>Highest Price</option></select>
+      </label>
+      <label>
+        <strong>Delivery Method:</strong>
+        <span><input type="checkbox" /> Quick Ship</span>
+      </label>
+    </div>
+    <div className={styles.buttons}>
+      <Button>Clear All</Button>
+    </div>
     {children}
-  </div>
+  </Section>
 );
 
 export default ProductController;
