d431ab36b18d0a57f1bb064b0c38561d5948655e
Clay Clecidor <clecidor@gmail.com>
2020-08-14 16:52:04 -0400

    Added SortFilterView to path /products/filter
    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import AppView from './views/AppView';
 import HomeView from './views/HomeView';
+import SortFilterView from './views/SortFilterView';
 
 
 function App() {
@@ -9,7 +10,9 @@ function App() {
     <Router>
       <Switch>
         <Route path="/app" component={AppView} />
-        <Route path="/" component={HomeView} />
+        <Route path="/products/filter" component={SortFilterView} />
+        <Route path="/products" component={HomeView} exact />
+        <Route path="/" component={HomeView} exact />
       </Switch>
     </Router>
   );
