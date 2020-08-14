665118d5a9433dec0d217e1174fa9b0bfa347f2b
Clay Clecidor <clecidor@gmail.com>
2020-08-14 18:25:58 -0400

    new file:   src/components/ExpandIcon/component.test.tsx new file:   src/components/ExpandIcon/images/MinusIcon.svg new file:   src/components/ExpandIcon/images/PlusIcon.svg new file:   src/components/ExpandIcon/index.tsx new file:   src/components/ExpandIcon/styles.module.scss

---
+++ src/components/ExpandIcon/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import classnames from 'classnames';
+import PlusIcon from './images/PlusIcon.svg';
+import MinusIcon from './images/MinusIcon.svg';
+import styles from './styles.module.scss';
+
+
+type Props = {
+  open?: boolean;
+  onClick?: React.MouseEventHandler<any>
+  className?: string;
+  children?: React.ReactNode;
+};
+
+export const ExpandIcon = ({ open, onClick, className, children, ...props } : Props) => (
+  <i className={classnames(styles.ExpandIcon, styles.className)} onClick={onClick} {...props}>
+    <img src={!open ? PlusIcon : MinusIcon} alt="Expand Button" />
+    {children}
+  </i>
+);
+
+export default ExpandIcon;
