71d9bda1b7a564eaddf9d6b5519b3dc698772dc9
Clay Clecidor <clecidor@gmail.com>
2020-08-14 17:20:27 -0400

    Added Mobile support to ProductController
    modified:   src/components/ProductController/index.tsx
    modified:   src/components/ProductController/styles.module.scss

--- src/components/ProductController/index.tsx
+++ src/components/ProductController/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import Button from '../Button';
 import Section from '../Section';
 import Selector from '../Selector';
@@ -10,26 +11,32 @@ type Props = {
 };
 
 const ProductController = ({ children } : Props) => (
-  <Section className={styles.ProductController}>
-    <div className={styles.options}>
-      <label>
-        <strong>Select Appliances:</strong>
-        <Selector><option>4 Appliances Selected</option></Selector>
-      </label>
-      <label>
-        <strong>Sort By:</strong>
-        <Selector><option>Highest Price</option></Selector>
-      </label>
-      <label>
-        <strong>Delivery Method:</strong>
-        <span><input type="checkbox" /> Quick Ship</span>
-      </label>
-    </div>
-    <div className={styles.buttons}>
-      <Button>Clear All</Button>
-    </div>
-    {children}
-  </Section>
+  <div className={styles.ProductController}>
+    <Section className={styles.MobileController}>
+      <Link to="/products/filter"><Button className={styles.button}>Sort & Filter</Button></Link>
+    </Section>
+
+    <Section className={styles.DesktopController}>
+      <div className={styles.options}>
+        <label>
+          <strong>Select Appliances:</strong>
+          <Selector><option>4 Appliances Selected</option></Selector>
+        </label>
+        <label>
+          <strong>Sort By:</strong>
+          <Selector><option>Highest Price</option></Selector>
+        </label>
+        <label>
+          <strong>Delivery Method:</strong>
+          <span><input type="checkbox" /> Quick Ship</span>
+        </label>
+      </div>
+      <div className={styles.buttons}>
+        <Button>Clear All</Button>
+      </div>
+      {children}
+    </Section>
+  </div>
 );
 
 export default ProductController;
