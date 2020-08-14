6d2a95ed3ed22204b3d1b9dd25eff6626f041b58
Clay Clecidor <clecidor@gmail.com>
2020-08-14 12:36:26 -0400

    Set Header max-width=950px
    modified:   src/views/HomeView/index.tsx
    modified:   src/views/HomeView/styles.module.scss

--- src/views/HomeView/index.tsx
+++ src/views/HomeView/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import styles from './styles.module.scss';
 import Header from '../../components/Header';
 import Section from '../../components/Section';
 import ProductView from '../ProductView';
+import styles from './styles.module.scss';
 
 
 type Props = {
@@ -12,9 +12,9 @@ type Props = {
 const HomeView = ({ children } : Props) => (
   <main className={styles.HomeView}>
     <Section>
-      <Header title="Appliance Packages">
-        Looking for a great deal on home appliances? Packages are the best bet. Most appliance packages consist<br />
-        of a range, refrigerator, over-the-range microwave, and dishwasher. However, some upscale brands have <br />
+      <Header title="Appliance Packages" className={styles.header}>
+        Looking for a great deal on home appliances? Packages are the best bet. Most appliance packages consist
+        of a range, refrigerator, over-the-range microwave, and dishwasher. However, some upscale brands have
         packages that incorporate wall ovens, cooktops, and integrated refrigerators.
       </Header>
     </Section>
