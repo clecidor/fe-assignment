00124d658bb684e8ee5f0f5c0a5671d11534fb7e
Clay Clecidor <clecidor@gmail.com>
2020-08-14 08:39:02 -0400

    Refactor App.tsx to use BrowserRouter moved: src/App.* /src/views/AppView
    modified:   src/App.tsx
    renamed:    src/App.test.tsx -> src/views/AppView/component.test.tsx
    renamed:    src/logo.svg -> src/views/AppView/images/logo.svg
    new file:   src/views/AppView/index.tsx
    renamed:    src/App.css -> src/views/AppView/styles.module.scss

---
+++ src/views/AppView/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './images/logo.svg';
+import styles from './styles.module.scss';
+
+const HomeView = () => (
+  <div className={styles["App"]}>
+    <header className={styles["App-header"]}>
+      <img src={logo} className={styles["App-logo"]} alt="logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        className={styles["App-link"]}
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
+
+export default HomeView;
