caf245870ebbd2ef9541f0011fd97c52c05a25d9
Clay Clecidor <clecidor@gmail.com>
2020-08-14 10:32:49 -0400

    Added Section wrapper
    modified:   src/views/HomeView/index.tsx
    modified:   src/views/HomeView/styles.module.scss

--- src/views/HomeView/index.tsx
+++ src/views/HomeView/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './styles.module.scss';
 import Header from '../../components/Header';
+import Section from '../../components/Section';
 import ProductView from '../ProductView';
 
 
@@ -10,11 +11,13 @@ type Props = {
 
 const HomeView = ({ children } : Props) => (
   <main className={styles.HomeView}>
-    <Header title="Appliance Packages">
-      Looking for a great deal on home appliances? Packages are the best bet.
-      Most appliance packages consist of a range, refrigerator, over-the-range microwave, and dishwasher.
-      However, some upscale brands have packages that incorporate wall ovens, cooktops, and integrated refrigerators.
-    </Header>
+    <Section>
+      <Header title="Appliance Packages">
+        Looking for a great deal on home appliances? Packages are the best bet. Most appliance packages consist<br />
+        of a range, refrigerator, over-the-range microwave, and dishwasher. However, some upscale brands have <br />
+        packages that incorporate wall ovens, cooktops, and integrated refrigerators.
+      </Header>
+    </Section>
     <ProductView />
     {children}
   </main>
