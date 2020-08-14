35fb531c16774ed73db119a218200dc95ee856aa
Clay Clecidor <clecidor@gmail.com>
2020-08-14 14:42:24 -0400

    new file:   src/components/Selector/component.test.tsx new file:   src/components/Selector/images/ArrowDown.svg new file:   src/components/Selector/index.tsx new file:   src/components/Selector/styles.module.scss

---
+++ src/components/Selector/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import classnames from 'classnames';
+import ArrowDown from './images/ArrowDown.svg';
+import styles from './styles.module.scss';
+
+
+type Props = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export const Selector = ({ className, children, ...props }: Props) => (
+  <div className={classnames(styles.Selector, className)} {...props}>
+    <select className={styles.select}>
+      {children}
+    </select>
+    <img src={ArrowDown} className={styles.arrow} alt="" />
+  </div>
+);
+
+export default Selector;
