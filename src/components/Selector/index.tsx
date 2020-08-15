c64a3cd75c88e18bc055b3ca9f6a144e70dd6419
Clay Clecidor <clecidor@gmail.com>
2020-08-14 21:57:54 -0400

    Added onChange handler
    modified:   src/components/Selector/index.tsx
    modified:   src/components/Selector/styles.module.scss

--- src/components/Selector/index.tsx
+++ src/components/Selector/index.tsx
@@ -7,11 +7,12 @@ import styles from './styles.module.scss';
 type Props = {
   children: React.ReactNode;
   className?: string;
+  onChange?: (event : React.ChangeEvent<HTMLSelectElement>) => void;
 };
 
-export const Selector = ({ className, children, ...props }: Props) => (
+export const Selector = ({ className, children, onChange, ...props }: Props) => (
   <span className={classnames(styles.Selector, className)} {...props}>
-    <select className={styles.select}>
+    <select className={styles.select} onChange={onChange}>
       {children}
     </select>
     <img src={ArrowDown} className={styles.arrow} alt="" />
